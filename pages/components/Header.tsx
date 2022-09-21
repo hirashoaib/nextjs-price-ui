d9f0cdd2ca4a790f7830ba0ca6c237612f6965aa
hirashoaib <hira@panacloud.com>
2022-09-22 00:02:06 +0500

    Made it Responsive

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -5,17 +5,17 @@ export const Header = () => {
   return (
     <Box pb="28" as="section">
       <Box
-        
-        color="#F7FAFC"
-        bg="#6B46C1"
+        textAlign={["left","left","center"]}
+        color="gray.200"
+        bg="#24305E"
         pt="90px"
         pb="198px"
         px="8"
       >
-        <Heading fontWeight="extrabold" fontSize="5xl">
+        <Heading fontWeight="extrabold" fontSize={["3xl","3xl","5xl"]}>
           Simple Pricing for your business
         </Heading>
-        <Text fontWeight="medium" fontSize="2xl" pt="4">
+        <Text fontWeight="medium" fontSize={["lg","lg","2xl"]} pt="4">
           Plans that are carefully crafted to suit your business.
         </Text>
       </Box>
