764346d883470ba6ff48f164c555b4c511844f57
hirashoaib <hira@panacloud.com>
2022-09-21 21:06:32 +0500

    Created Pricing Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Header } from './components/Header'
+import { Pricing } from './components/Pricing'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Header/>
-
+      <Pricing/>
       
     </div>
   )
