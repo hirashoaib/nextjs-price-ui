530852e27a26cbe86b8e51c8e7501cfadcf5ae37
hirashoaib <hira@panacloud.com>
2022-09-21 23:23:10 +0500

    Implemented default theme

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -3,19 +3,19 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <Box pb="112px" as="section">
+    <Box pb="28" as="section">
       <Box
         
         color="#F7FAFC"
         bg="#6B46C1"
         pt="90px"
         pb="198px"
-        px="32px"
+        px="8"
       >
-        <Heading fontWeight="800" fontSize="48px">
+        <Heading fontWeight="extrabold" fontSize="5xl">
           Simple Pricing for your business
         </Heading>
-        <Text fontWeight="500" fontSize="24px" pt="16px">
+        <Text fontWeight="medium" fontSize="2xl" pt="4">
           Plans that are carefully crafted to suit your business.
         </Text>
       </Box>
